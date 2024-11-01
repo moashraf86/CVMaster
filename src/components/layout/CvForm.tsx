5fb7c42c2782f4e48e81a4ed0ea0e986fe24f8f1
moashraf86 <botoashraf123@gmail.com>
2024-11-01 17:59:30 +0200

    add HeaderForm and SummaryForm components for user input collection

---
+++ src/components/layout/CvForm.tsx
@@ -0,0 +1,11 @@
+import { HeaderForm } from "../core/HeaderForm";
+import { SummaryForm } from "../core/SummaryForm";
+
+export const CvForm = () => {
+  return (
+    <div className="w-1/2">
+      <HeaderForm />
+      <SummaryForm />
+    </div>
+  );
+};
