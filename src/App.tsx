51eb7384597a07365145a36eb993cd8db461f95e
moashraf86 <botoashraf123@gmail.com>
2024-11-10 05:25:55 +0200

    feat: add Skills section with form and preview components; enhance navigation and form state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { CvForm } from "./components/layout/CvForm";
 import { CvPreview } from "./components/layout/CvPreview";
-import { FormProvider } from "./store/useFormContext";
 
 function App() {
   return (
-    <div className="container-fluid flex gap-10 p-10">
-      <CvForm />
-      <CvPreview />
+    <div className="container-fluid p-10">
+      <div className="flex gap-10">
+        <CvForm />
+        <CvPreview />
+      </div>
     </div>
   );
 }
